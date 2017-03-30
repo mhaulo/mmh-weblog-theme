8170cf2579f898f8b1a1fd60b4173be4c8ed80c7
Mika Haulo <mika@haulo.fi>
2017-03-30 22:58:04 +0300

    Tweak content styles

--- template-parts/content-aside.php
+++ template-parts/content-aside.php
@@ -11,74 +11,26 @@
 
 <?php global $post_index; ?>
 
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<?php if ( is_single() ) : ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php the_post_thumbnail(); ?>
-
+	<h2 class="entry-title">
+		<a href="<?php echo esc_url( get_permalink() ); ?>">
+			<time><?php echo get_the_date(); ?></time>
 			<?php
-
-				echo '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . get_post()->post_title .  '</a></h2>';
-
-                                if ( 'post' === get_post_type() ) : ?>
-                                        <div class="entry-meta">
-                                                <?php mmh_weblog_posted_on(); ?>
-                                        </div><!-- .entry-meta -->
-                                <?php endif; ?>
-				
-				<?php the_meta(); ?>
-
-
-		</header><!-- .entry-header -->
-
-		<div class="entry-content">
-			<?php
-				the_content( sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'mmh-weblog' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
-
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mmh-weblog' ),
-					'after'  => '</div>',
-				) );
+				if ( get_post_status ( $ID ) == 'private' ) {
+					echo '- <span style="font-size: 65%;">&nbsp;&nbsp;<i class="fa fa-lock"></i></span>';
+				}
 			?>
-		</div><!-- .entry-content -->
 
-		<footer class="entry-footer">
-			<?php mmh_weblog_entry_footer(); ?>
-		</footer><!-- .entry-footer -->
-	</article><!-- #post-## -->
+			<br>
+			<?php echo get_post()->post_title; ?>
+		</a>
+	</h2>
 
-<?php else : ?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		the_post_thumbnail();
+		the_content();
+		the_meta();
+	?>
 
-		<?php
-			// Fetch title without the Private/Protected prefix if there's one
-			echo '<h2 class="entry-title"><span class="entry-meta">Päiväkirja <time>' . get_the_date() . ' </time><br> ';
-
-			echo '</span>';
-
-			echo '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . get_post()->post_title . '</a>';
-
-			if ( get_post_status ( $ID ) == 'private' ) {
-				echo '<span style="font-size: 65%;">&nbsp;&nbsp;<i class="fa fa-lock"></i></span>';
-			}
-
-			echo'</h2>';
-
-			the_meta();
-
-			the_post_thumbnail();
-
-			the_content();
-		?>
-
-		</article>
-
-
-
-<?php endif; // is_single() ?>
-
+</article>
--- template-parts/content-status.php
+++ template-parts/content-status.php
@@ -11,60 +11,24 @@
 
 <?php global $post_index; ?>
 
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<?php if ( is_single() ) : ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php if ( 'post' === get_post_type() ) : ?>
-				<div class="entry-meta">
-					<?php mmh_weblog_posted_on(); ?>
-				</div><!-- .entry-meta -->
-
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-				<?php the_post_thumbnail(); ?>
-
-			<?php endif; ?>
-
-		</header><!-- .entry-header -->
-
-		<div class="entry-content">
+	<h2 class="entry-title">
+		<a href="<?php echo esc_url( get_permalink() ); ?>">
+			<time><?php echo get_the_date(); ?></time>
 			<?php
-				/*the_content( sprintf(
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'mmh-weblog' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );*/
-
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mmh-weblog' ),
-					'after'  => '</div>',
-				) );
+				if ( get_post_status ( $ID ) == 'private' ) {
+					echo '- <span style="font-size: 65%;">&nbsp;&nbsp;<i class="fa fa-lock"></i></span>';
+				}
 			?>
-		</div><!-- .entry-content -->
 
-		<footer class="entry-footer">
-			<?php mmh_weblog_entry_footer(); ?>
-		</footer><!-- .entry-footer -->
-	</article><!-- #post-## -->
-
-<?php else : ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<br>
+			<?php echo get_post()->post_title; ?>
+		</a>
+	</h2>
 
 	<?php
-		echo '<h2 class="entry-title"><span class="entry-meta"><time>' . get_the_date() . ' </time>';
-
-                echo '</span><br>';
-
-		echo '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . get_post()->post_title . '</a>';
-                if ( get_post_status ( $ID ) == 'private' ) {
-	                echo '&nbsp;&nbsp;<span style=" font-size: 65%;"><i class="fa fa-lock"></i></span>';
-                }
-
-		echo '</h2>';
-
 		the_post_thumbnail();
 	?>
 
-	</article>
-
-<?php endif; // is_single() ?>
-
+</article>
